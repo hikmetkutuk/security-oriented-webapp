5cfe726ec2542887e202cd518a42adc4a97dc8f0
Hikmet Kütük <hikmetkutuk@gmail.com>
2025-08-18 16:53:59 +0300

    feat: add SecurityConfig class for HTTP security configuration

---
+++ backend/src/main/java/com/security/oriented/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.security.oriented.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((req) -> req.anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
